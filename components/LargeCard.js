9bcf835f8e7f498d7a96b7cc701fb880e05f7d26
Akbar Sha S <iamakbarsha1@gmail.com>
2021-08-14 00:52:41 +0530

    initial commit

---
+++ components/LargeCard.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+function LargeCard({ img, title, description, buttonText }) {
+   return (
+      <section className="relative py-8 cursor-pointer">
+
+         <div className="relative h-96 min-w-[300px]">
+            <Image
+               className="rounded-2xl"
+               src={img}
+               layout="fill"
+               objectFit="cover"
+            />
+         </div>
+
+         <div className="absolute top-16 left-9 md:top-20 lg:top-24 xl:top-24 2xl:top-32 md:left-10 lg:left-12 xl:left-14 2xl:left-14">
+            <h3 className="text-4xl mb-3 w-64 font-semibold">{title}</h3>
+            <p className="">{description}</p>
+            <button className="text-sm cursor-pointer bg-gray-900 rounded-md px-4 py-2 font-semibold text-white mt-5">{buttonText}</button>
+         </div>
+
+      </section>
+   )
+}
+
+export default LargeCard
---
+++ components/SmallCard.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+function SmallCard({ img, location, distance }) {
+   return (
+      <div className="flex items-center mt-4 m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out sm:m-3">
+         {/* left div */}
+         <div className="relative h-16 w-16">
+            <Image
+               src={img}
+               layout="fill"
+               className="rounded-lg"
+            />
+         </div>
+
+         {/* right div */}
+         <div>
+            <h2 className="text-gray-600 font-bold">{location}</h2>
+            <h3 className="text-gray-500">{distance}</h3>
+         </div>
+
+      </div>
+   )
+}
+
+export default SmallCard
